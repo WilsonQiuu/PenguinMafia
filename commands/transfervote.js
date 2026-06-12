f846a94964350f32e2dfbd8dafb48e1cddf9bf5f
Wilson Qiu <your_email@example.com>
2026-06-12 13:41:35 -0400

    Remove singular transfer vote command

--- commands/transfervote.js
+++
@@ -1,9 +0,0 @@
-const {
-    buildCommand,
-    executeTransferVotes
-} = require('./transfervotes.js');
-
-module.exports = {
-    data: buildCommand('transfervote'),
-    execute: executeTransferVotes
-};
